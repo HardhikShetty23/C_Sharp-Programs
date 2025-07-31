6a0323a7f2b3accdc8fd533102356934d8e8b8d3
HardhikShetty23 <hardhik650@gmail.com>
2025-07-31 15:24:31 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,8 +4,17 @@ class Program
 {
     static void Main()
     {
-        int number = 5;
-        int square = number * number;
-        Console.WriteLine("Square of " + number + " is " + square);
+        int number = 12345;
+        int sum = 0;
+        int temp = number;
+
+        while (temp > 0)
+        {
+            int digit = temp % 10;
+            sum += digit;
+            temp /= 10;
+        }
+
+        Console.WriteLine("Sum of digits of " + number + " is " + sum);
     }
 }
